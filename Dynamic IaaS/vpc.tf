877ceaad33ea1e819796c05ae079af23c6267ff0
Sujagi Verma <66088555+Sujagi02@users.noreply.github.com>
2021-05-27 01:41:40 +0530

    Update vpc.tf

--- Dynamic IaaS/vpc.tf
+++ Dynamic IaaS/vpc.tf
@@ -1,1 +1,60 @@
+#Creating a VPC
+resource "aws_vpc" "my_vpc" {
+    cidr_block = var.vpc_cidr
 
+    tags = {
+       Name = "My VPC"
+}
+}
+
+#Creating IN Gateway
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.my_vpc.id
+
+    tags = {
+       Name = "My gw"
+}
+}
+
+#Creating Subnets
+resource "aws_subnet" "my_subnet" {
+  count = length(var.subnet_cidr)
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = element(var.subnet_cidr,count.index)
+  availability_zone= element(var.azs,count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+      Name = "Subnet-${count.index+1}"
+  }
+}
+
+#Creating Route tables
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+        Name = "RT"
+    }
+}
+
+#Associating RT
+resource "aws_route_table_association" "rt_ass" {
+  count = length(var.subnet_cidr)
+  subnet_id      = element(aws_subnet.my_subnet.*.id,count.index)
+  route_table_id = aws_route_table.rt.id
+}
+
+#Creating an instance
+resource "aws_instance" "ec2" {
+	   ami = var.ami
+	   instance_type = var.ec2_type
+	   tags = {
+	        Name = "Iaas_OS"	
+                }
+}
